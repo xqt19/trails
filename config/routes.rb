b43ba6927275f450c5b527dcfa1e096c71950f73
Zache He <zet_he@hotmail.com>
2019-12-03 10:47:46 +0800

    added activities controller and resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :trails
+  resources :trails do
+    resources :activities, except: [:index]
+  end
 end
