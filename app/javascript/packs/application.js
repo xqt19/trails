70279f9913bbd7123fdb09ef243c6d9a8876faca
Zache He <zet_he@hotmail.com>
2019-12-08 22:10:31 +0800

    able to check each delegation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,15 @@ import { initSweetalert } from '../plugins/init_sweetalert';
 import { changeCategory } from '../components/button';
 import { createCustom } from '../components/custom';
 import { submitButtonForm } from '../components/submit-button-form';
+import { checkListItems, checkDelegates } from '../components/checkbox';
 
 const categoryItems = document.querySelector('.category-items');
 const customItems = document.querySelector('.category-items');
 const customBtn = document.querySelector('.custom-btn');
 const form = document.getElementById('new_activity');
+
+const checkboxes = document.querySelectorAll('.checkbox');
+const delegates = document.querySelectorAll('.delegate-checkbox');
 
 initSelect2();
 
@@ -26,4 +30,12 @@ if (form) {
   submitButtonForm();
 }
 
-export { categoryItems, customItems, customBtn, form };
+if (checkboxes) {
+  checkListItems();
+}
+
+if (delegates) {
+  checkDelegates();
+}
+
+export { categoryItems, customItems, customBtn, form, checkboxes, delegates };
--- app/models/delegation.rb
+++ app/models/delegation.rb
@@ -3,4 +3,8 @@ class Delegation < ApplicationRecord
   belongs_to :list_item
 
   validates :user, uniqueness: { scope: :list_item }
+
+  def checked?
+    checked
+  end
 end
